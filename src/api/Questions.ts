f56a18bef819fc5ab54b10a1795fcbc49157a708
Ajayi Segun <ajayisegun2003@gmail.com>
2025-05-25 16:37:03 +0100

    how is it na again

--- src/api/Questions.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-
-export const GetAssementApi = async()=>{
-  try {
-    
-  } catch (error) {
-    
-  }
-}
--- src/hooks/useQuestions.ts
+++ src/hooks/useQuestions.ts
@@ -4,9 +4,8 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { setQuestions } from "../store/assessmentSlice";
 
-const URL = "https://tech-assessment-backend.onrender.com/api"
 
-const fetcher = (url: string) => axios.get(`https://tech-assessment-backend.onrender.com/api/question/get-all-questions`).then(res => res.data.questions);
+const fetcher = () => axios.get(`https://tech-assessment-backend.onrender.com/api/question/get-all-questions`).then(res => res.data.questions);
 
 export const useQuestions = () => {
   const dispatch = useDispatch();
